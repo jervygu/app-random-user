3483571219dbc71f92134107f75b19325b5b1c96
Jervy Umandap <jegumandap@unionbankph.com>
2024-07-18 10:59:17 +0800

    Delete Main.storyboard and ViewController, scene delegate and Setting up MainViewController

---
+++ Random_User/Random_User/MainViewController/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  Random_User
+//
+//  Created by Jervy Umandap on 7/18/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
